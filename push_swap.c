2912ea3fbf41afdc3b9363565001ec5264db36e8
Fayiz Asad <fayizasad@gmail.com>
2022-02-16 17:42:51 +0400

    added instructions

--- push_swap.c
+++ push_swap.c
@@ -66,6 +66,16 @@ void input_to_stack(char **input, int *stack_a)
   }
 }
 
+void init_stack(int *stack, int count)
+{
+  while (count > 0)
+  {
+    *stack = 0;
+    *stack++;
+    count--;
+  }
+}
+
 void print_stack(int *stack, int count)
 {
   int i;
@@ -73,7 +83,7 @@ void print_stack(int *stack, int count)
   i = 0;
   while(count > 0)
   {
-    printf("%d",stack[i]);
+    printf("%d\n",stack[i]);
     i++;
     count--;
   }
@@ -90,7 +100,96 @@ void swap(int *stack, int pri, int sec)
   stack[sec] = temp;
 }
 
+int check_s(int *stack, int count)
+{
+  int len;
 
+  len = 0;
+  while (count > 0)
+  {
+    len++;
+    *stack++;
+    count--;
+  }
+  if (len == 0)
+    return 0;
+  else if (len == 1)
+    return 1;
+  return 2;
+}
+
+void sa_sb(int *stack, int count)
+{
+  int temp;
+
+  temp = 0;
+  if(check_s(stack, count) == 2)
+  {
+    temp = stack[0];
+    stack[0] = stack[1];
+    stack[1] = temp;
+  }
+}
+
+void pa_pb(int *stack_1, int *stack_2, int count)
+{
+  int i;
+
+  i = 0;
+  if (check_s(stack_1, count) != 0)
+  {
+    stack_2[0] = stack_1[0];
+    while(count > 0)
+    {
+      stack_1[i] = stack_1[i+1];
+      i++;
+      count--;
+    }
+    stack_1[i - 1] = 0;
+  }
+}
+
+void ra_rb(int *stack, int count)
+{
+  int first;
+  int last;
+  int i;
+  int j;
+
+  i = 0;
+  j = 1;
+  last = count - 1;
+  first = stack[0];
+  while (count > 0)
+  {
+    stack[i] = stack[j];
+    i++;
+    j++;
+    count--;
+  }
+  stack[last] = first;
+}
+
+void rra_rrb(int *stack, int count)
+{
+  int i;
+  int j;
+  int last;
+  int count_1;
+
+  i = 0;
+  j = count - 2;
+  last = stack[count - 1];
+  count_1 = count - 1;
+  while(count > 0)
+  {
+    stack[count_1] = stack[j];
+    j--;
+    count_1--;
+    count--;
+  }
+  stack[0] = last;
+}
 
 void sort_stack(int *stack_a, int count)
 {
@@ -108,8 +207,7 @@ void sort_stack(int *stack_a, int count)
     primary = stack_a[i];
     j = i + 1;
     secondary_count = count - 1;
-    wh
-    ile(primary > stack_a[j])
+    while(primary > stack_a[j])
     {
       swap(stack_a, i ,j);
       j++;
@@ -123,11 +221,17 @@ void sort_stack(int *stack_a, int count)
 void main(int argc, char **argv)
 {
   int stack_a[count_input(argv)];
+  int stack_b[count_input(argv)];
   int count;
 
   count = count_input(argv);
   input_to_stack(argv, stack_a);
-  sort_stack(stack_a, count);
-  sort_stack(stack_a, count);
-  print_stack(stack_a, count); 
+  init_stack(stack_b, count);
+  // sort_stack(stack_a, count);
+  // sa_sb(stack_a, count);
+  pa_pb(stack_a, stack_b, count);
+  printf("stack_a:\n");
+  print_stack(stack_a, count);
+  printf("stack_b:\n");
+  print_stack(stack_b, count); 
 }
